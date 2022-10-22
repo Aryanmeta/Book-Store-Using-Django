a3ac0472b8d537c02254a96ee616a47b3cc2b8ec
aryan zarei <aryan2020.zarei@gmail.com>
2022-10-22 15:04:15 +0330

    Create Book model

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+
+# @admin.register(Book)
+# class Book(admin.ModelAdmin):
+#     list_display = ('title', 'author', 'price', )
+
+
+admin.site.register(Book)
