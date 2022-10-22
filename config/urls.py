fa0592fac10d4022fe6fbf3c2a11d4068cf58093
aryan zarei <aryan2020.zarei@gmail.com>
2022-10-22 22:01:40 +0330

    add Book-List-View

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
+    path('books/', include('books.urls')),
 ]
