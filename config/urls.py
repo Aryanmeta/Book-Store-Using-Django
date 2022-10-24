857947f72c003497880f8e05180e8f1bee58c2d2
aryan zarei <aryan2020.zarei@gmail.com>
2022-10-24 19:20:09 +0330

    install pillow package and add cover field to db in models.py

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +25,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('books/', include('books.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
