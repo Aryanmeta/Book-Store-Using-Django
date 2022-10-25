ea4340f3c85908886ce15588aa7104628c2f2f9f
aryan zarei <aryan2020.zarei@gmail.com>
2022-10-25 22:46:57 +0330

    create pagination

--- books/views.py
+++ books/views.py
@@ -6,6 +6,7 @@ from .models import Book
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 2
     template_name = 'books/book_list.html'
     context_object_name = 'books'
 
