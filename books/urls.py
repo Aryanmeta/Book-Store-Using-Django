dc25e7f2d525a5c24c17a280533ded36544ba36e
aryan zarei <aryan2020.zarei@gmail.com>
2022-10-26 10:10:00 +0330

    show comment in book-detail-view

--- books/urls.py
+++ books/urls.py
@@ -3,7 +3,7 @@ from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView,
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
-    path('<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('<int:pk>/', BookDetailView, name='book_detail'),
     path('create/', BookCreateView.as_view(), name='book_create'),
     path('<int:pk>/update/', BookUpdateView.as_view(), name='book_update'),
     path('<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
