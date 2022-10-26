8e8135c8aa827a6e4c26442005b843379259f145
aryan zarei <aryan2020.zarei@gmail.com>
2022-10-26 16:41:40 +0330

    update Comment models

--- books/models.py
+++ books/models.py
@@ -22,6 +22,8 @@ class Comment(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments')
     text = models.TextField()
     datetime_created = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    recommend = models.BooleanField(default=True)
 
     def __str__(self):
         return self.text
