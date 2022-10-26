ee039b53d7aa5db7665e5885e5861e62a8224f1e
aryan zarei <aryan2020.zarei@gmail.com>
2022-10-26 09:26:37 +0330

    register comment models in admin

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Comment
 
 
 # @admin.register(Book)
@@ -8,3 +8,4 @@ from .models import Book
 
 
 admin.site.register(Book)
+admin.site.register(Comment)
